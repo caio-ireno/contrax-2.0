687dd47615c6cff2b01aab6331bb5689fc6c27ad
caio-ireno <caio_ireno@hotmail.com>
2023-04-27 17:05:46 -0300

    fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { Box } from "native-base";
+import { Loading } from "./src/components/Loading";
 import { AppThemeProvider } from "./src/context/ThemeContext";
 import { Login } from "./src/screens/Login";
+import { useFonts, Roboto_400Regular } from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular });
   return (
-    <AppThemeProvider>
-      <Login />
-    </AppThemeProvider>
+    <AppThemeProvider>{fontsLoaded ? <Login /> : <Loading />}</AppThemeProvider>
   );
 }
